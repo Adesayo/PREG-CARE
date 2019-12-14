c95164908d71a7a3fa546259c2f2700a22a1f57a
Adesayo <53871688+Adesayo@users.noreply.github.com>
2019-12-14 01:33:23 +0100

    Add files via upload

---
+++ pregcare.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+  jQuery(".trigger").click(function(){
+    jQuery(".nav-a").slideToggle();
+  });
+});
+
+jQuery(document).ready(function(){
+  jQuery(".toggle").click(function(){
+    jQuery(".nav-b").slideToggle();
+  });
+});
